6661c15e6e0c70e4b117392d8af0901637ecf9b8
basecamp <cvanderpoel@basecampcodingacademy.org>
2018-10-12 11:20:00 -0500

    I made the data that I will eventually need for my page

---
+++ javascript.js
@@ -0,0 +1,19 @@
+const PAGE_DATA = {
+    vehicle: {
+        truck: {
+            name: "truck",
+            stock: 5,
+            price: 25
+        },
+        sedan: {
+            name: "sedan",
+            stock: 5,
+            price: 15
+        },
+        van: {
+            name: "van",
+            stock: 5,
+            price: 20
+        }
+    }
+};
