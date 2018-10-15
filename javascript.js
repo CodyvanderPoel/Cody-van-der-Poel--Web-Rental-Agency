360669a6700da12b5f5d2efff50954c5fc5d55cc
basecamp <cvanderpoel@basecampcodingacademy.org>
2018-10-15 09:09:28 -0500

    I changed my Page Data and added extra data to it

--- javascript.js
+++ javascript.js
@@ -1,19 +1,40 @@
 const PAGE_DATA = {
-    vehicle: {
-        truck: {
-            name: "truck",
+    games: {
+        fallout76: {
+            name: "Fallout 76",
+            image: "images/220px-Fallout_76_cover.jpg",
             stock: 5,
             price: 25
         },
-        sedan: {
-            name: "sedan",
+        redDeadRedemption2: {
+            name: "Red Dead Redemption 2",
+            image: "images/Red-Dead-Redemption-2.jpeg",
             stock: 5,
-            price: 15
+            price: 25
         },
-        van: {
-            name: "van",
+        monsterHunterWorld: {
+            name: "Monster Hunter World",
+            image: "images/220px-Monster_Hunter_World_cover_art.jpg",
             stock: 5,
             price: 20
+        },
+        skyrim: {
+            name: "The Elder Scrolls V: Skyrim",
+            image: "images/skyrim.jpeg",
+            stock: 5,
+            price: 5
+        },
+        fallout4: {
+            name: "Fallout 4",
+            image: "images/Fallout-4-cover-art.jpg",
+            stock: 5,
+            price: 10
+        },
+        witcher3: {
+            name: "The Witcher 3: The Wild Hunt",
+            image: "images/220x-Witcher_3_cover_art.jpg",
+            stock: 5,
+            price: 15
         }
     }
 };
