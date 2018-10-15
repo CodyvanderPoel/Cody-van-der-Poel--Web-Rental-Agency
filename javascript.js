258b553e3cd3c055cd29504cb6fcdd907e312683
basecamp <cvanderpoel@basecampcodingacademy.org>
2018-10-15 15:50:49 -0500

    I edited my object to match the requirements set by my newly made template function

--- javascript.js
+++ javascript.js
@@ -1,40 +1,54 @@
 const PAGE_DATA = {
-    games: {
-        fallout76: {
+    games: [
+        {
             name: "Fallout 76",
             image: "images/220px-Fallout_76_cover.jpg",
             stock: 5,
             price: 25
         },
-        redDeadRedemption2: {
+        {
             name: "Red Dead Redemption 2",
             image: "images/Red-Dead-Redemption-2.jpeg",
             stock: 5,
             price: 25
         },
-        monsterHunterWorld: {
+        {
             name: "Monster Hunter World",
             image: "images/220px-Monster_Hunter_World_cover_art.jpg",
             stock: 5,
             price: 20
         },
-        skyrim: {
+        {
             name: "The Elder Scrolls V: Skyrim",
             image: "images/skyrim.jpeg",
             stock: 5,
             price: 5
         },
-        fallout4: {
+        {
             name: "Fallout 4",
             image: "images/Fallout-4-cover-art.jpg",
             stock: 5,
             price: 10
         },
-        witcher3: {
+        {
             name: "The Witcher 3: The Wild Hunt",
             image: "images/220x-Witcher_3_cover_art.jpg",
             stock: 5,
             price: 15
         }
-    }
+    ]
 };
+
+function renderItems() {
+    var source = document.getElementById("stockTemplate").innerHTML;
+    var template = Handlebars.compile(source);
+    var data = PAGE_DATA.games;
+    for (game in data) {
+        var html = template({
+            name: data[game].name,
+            image: data[game].image
+        });
+        document.getElementById("items").insertAdjacentHTML("afterbegin", html);
+    }
+}
+renderItems();
