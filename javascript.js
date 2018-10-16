d713434add0880c70de4f57ccc3e0f69e9a2edb7
basecamp <cvanderpoel@basecampcodingacademy.org>
2018-10-16 10:37:44 -0500

    I changed the order of my object's contents

--- javascript.js
+++ javascript.js
@@ -1,6 +1,12 @@
 const PAGE_DATA = {
     game_set_1: [
         {
+            name: "Monster Hunter World",
+            image: "images/220px-Monster_Hunter_World_cover_art.jpg",
+            stock: 5,
+            price: 20
+        },
+        {
             name: "Fallout 76",
             image: "images/220px-Fallout_76_cover.jpg",
             stock: 5,
@@ -11,20 +17,14 @@ const PAGE_DATA = {
             image: "images/Red-Dead-Redemption-2.jpeg",
             stock: 5,
             price: 25
-        },
-        {
-            name: "Monster Hunter World",
-            image: "images/220px-Monster_Hunter_World_cover_art.jpg",
-            stock: 5,
-            price: 20
         }
     ],
     game_set_2: [
         {
-            name: "The Elder Scrolls V: Skyrim",
-            image: "images/skyrim.jpeg",
+            name: "The Witcher 3: The Wild Hunt",
+            image: "images/220px-Witcher_3_cover_art.jpg",
             stock: 5,
-            price: 5
+            price: 15
         },
         {
             name: "Fallout 4",
@@ -33,10 +33,10 @@ const PAGE_DATA = {
             price: 10
         },
         {
-            name: "The Witcher 3: The Wild Hunt",
-            image: "images/220px-Witcher_3_cover_art.jpg",
+            name: "The Elder Scrolls V: Skyrim",
+            image: "images/skyrim.jpeg",
             stock: 5,
-            price: 15
+            price: 5
         }
     ]
 };
