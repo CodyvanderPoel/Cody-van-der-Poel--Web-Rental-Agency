bc891c651fe54638826e08640814fbe0ece30eb2
basecamp <cvanderpoel@basecampcodingacademy.org>
2018-10-17 10:47:29 -0500

    I added descriptions to my objects and made the functions export them as well

--- javascript.js
+++ javascript.js
@@ -5,21 +5,27 @@ const PAGE_DATA = {
             image: "images/220px-Monster_Hunter_World_cover_art.jpg",
             stock: 5,
             price: 20,
-            id: "MHW"
+            id: "MHW",
+            description:
+                "Monster Hunter World introduces a living, breathing ecosystem in which players take on the role of a hunter that seeks and slays ferocious beasts in heart-pounding battles that unfold across the vast ever-changing terrain"
         },
         {
             name: "Fallout 76",
             image: "images/220px-Fallout_76_cover.jpg",
             stock: 5,
             price: 25,
-            id: "F76"
+            id: "F76",
+            description:
+                "Fallout 76 is an upcoming online multiplayer action role-playing video game developed by Bethesda Game Studios and published by Bethesda Softworks."
         },
         {
             name: "Red Dead Redemption 2",
             image: "images/Red-Dead-Redemption-2.jpeg",
             stock: 5,
             price: 25,
-            id: "RDR2"
+            id: "RDR2",
+            description:
+                "Red Dead Redemption 2 is a Western-themed action-adventure game set in an open world environment. It is played from either a third or first-person perspective and the player controls Arthur Morgan, an outlaw and a member of the Van der Linde gang."
         }
     ],
     game_set_2: [
@@ -28,21 +34,27 @@ const PAGE_DATA = {
             image: "images/220px-Witcher_3_cover_art.jpg",
             stock: 5,
             price: 15,
-            id: "TW3TWH"
+            id: "TW3TWH",
+            description:
+                "The Witcher 3: Wild Hunt is a story-driven, next-generation open world role-playing game, set in a visually stunning fantasy universe, full of meaningful choices and impactful consequences. You play as Geralt of Rivia, a monster hunter tasked with finding a child from an ancient prophecy."
         },
         {
             name: "Fallout 4",
             image: "images/Fallout-4-cover-art.jpg",
             stock: 5,
             price: 10,
-            id: "F4"
+            id: "F4",
+            description:
+                "Fallout 4 is a post-apocalyptic action role-playing video game developed by Bethesda Game Studios and published by Bethesda Softworks. The player explores the game's dilapidated world, complete various quests, help out factions, and acquire experience points to level up and increase the abilities of their character."
         },
         {
             name: "The Elder Scrolls V: Skyrim",
             image: "images/skyrim.jpeg",
             stock: 5,
             price: 5,
-            id: "TESVS"
+            id: "TESVS",
+            description:
+                "The Elder Scrolls V: Skyrim is an action role-playing video game developed by Bethesda Game Studios and published by Bethesda Softworks. The game's main story revolves around the player character's quest to defeat Alduin the World-Eater, a dragon who is prophesied to destroy the world. The game is set 200 years after the events of Oblivion and takes place in the fictional province of Skyrim."
         }
     ]
 };
@@ -57,7 +69,8 @@ function renderItemSetOne() {
             image: data[game].image,
             stock: data[game].stock,
             price: data[game].price,
-            id: data[game].id
+            id: data[game].id,
+            description: data[game].description
         });
         document
             .getElementById("item_set_1")
@@ -76,7 +89,8 @@ function renderItemSetTwo() {
             image: data[game].image,
             stock: data[game].stock,
             price: data[game].price,
-            id: data[game].id
+            id: data[game].id,
+            description: data[game].description
         });
         document
             .getElementById("item_set_2")
